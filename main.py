3d2a61b15a578e2d315536aea0834c600118b6ec
itsadi45 <107479416+itsadi45@users.noreply.github.com>
2022-06-14 23:01:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import urllib
 import os
 serverPort = 5050
 
-def startServer():
+def startNGROKServer():
+    time.sleep(1)
     os.system("wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
     os.system("unzip -qq -n ngrok-stable-linux-amd64.zip")
     os.system("ngrok authtoken 2AZ6Q4rkOlX4Lk8Wyl2p6LeSzm5_86zUyKaEBTBdMDf9nrQjV")
@@ -13,8 +14,10 @@ def startServer():
     with urllib.request.urlopen('http://localhost:4040/api/tunnels') as response:
         data = json.loads(response.read().decode())
         (host, port) = data['tunnels'][0]['public_url'][6:].split(':')
+        return (host,port)
     
-    
+def startLocalServer():
+    os.system("python3 -m http.server 5050")
     
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('localhost', serverPort))
