f4832ae6edbff06cfd031ae38db4a93740646455
itsadi45 <107479416+itsadi45@users.noreply.github.com>
2022-06-14 19:28:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from socket import *
+import threading
+import os
 serverPort = 5050
 
+def startServer():
+    os.system("wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
+    os.system("unzip -qq -n ngrok-stable-linux-amd64.zip")
+    os.system("ngrok authtoken ")
+    os.system("")
+    
+    
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('localhost', serverPort))
 
